829c20ca544e500c0c310dfa49a4224c6d129bff
bwhite04 <60192676+bwhite04@users.noreply.github.com>
2020-03-15 20:58:52 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 $(function(){
-$('.slider').slick({
+   $('.slider').slick({
     arrows: false,
     fade: true,
     autoplay: 3000,
-    dots: true,
-})
+    dots: true
 });
+
+$('.header-btn').on('click', function(){
+    $('.menu').addClass('active');
+});
+
+$('.close-btn').on('click', function(){
+    $('.menu').removeClass('active');
+});
+
+
+
+
+});
+
